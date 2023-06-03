9a4914a5127cc265b1c0c964c8d351703003804c
William <slicer7667@gmail.com>
2023-06-03 02:27:46 -0400

    Added text boxes and button to test React page

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleMessageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log('Button was clicked, info was submitted!', name, message);
+    // You can perform further actions like sending the data to an API or updating state
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React App Button Testing</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <input
+            type="text"
+            id="message"
+            value={message}
+            onChange={handleMessageChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
