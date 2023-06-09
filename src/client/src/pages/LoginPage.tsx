b6f860a8ed6dfeef3ee1de18ceef9cc0511c5433
your_username <arielleramgoolie27@gmail.com>
2023-06-09 00:23:54 -0400

    Added basic details

--- src/client/src/pages/LoginPage.tsx
+++ src/client/src/pages/LoginPage.tsx
@@ -4,7 +4,42 @@ type Props = {}
 
 function LoginPage({}: Props) {
   return (
-    <div>LoginPage</div>
+    <div className='grid bg-darkestGrey text-white h-screen w-full overflow-clip grid-cols-1 sm:grid-cols-2'>
+      <div className='hidden sm:block'>
+        <img 
+          className='object-cover w-full h-full'
+          src='https://images.unsplash.com/photo-1535385793343-27dff1413c5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80'
+          alt=''/>
+      </div>
+
+      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+        <form className='text-white w-full max-w-[300px] rounded-xl mx-auto p-8 px-8 '>
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
+            CALYPSO
+          </h1>
+          <div>
+              <label className="block mb-2 text-sm font-medium text-white ">Your email</label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@email.com"/>
+          </div>
+
+          <div>
+              <label className="block mb-2 text-sm font-medium text-white">Password</label>
+              <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+          </div>
+
+          <div className="space-y-4 md:space-y-6">
+            <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+          </div>
+
+          <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+          
+          <p className="text-sm font-light text-gray-500">
+              Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+          </p>
+
+        </form>
+      </div>
+    </div>
   )
 }
 
