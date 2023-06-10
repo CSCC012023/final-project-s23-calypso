c062c30af37a30d13debba1c977150b30caca23c
your_username <arielleramgoolie27@gmail.com>
2023-06-10 18:27:46 -0400

    updated navigation of login page

--- src/client/src/pages/LoginPage.tsx
+++ src/client/src/pages/LoginPage.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-type Props = {}
+function LoginPage() {
 
-function LoginPage({}: Props) {
   return (
     <div className='grid bg-darkestGrey text-white h-screen w-full overflow-scroll lg:overflow-clip grid-cols-1 sm:grid-cols-2'>
       <div className='hidden sm:block'>
@@ -12,28 +11,28 @@ function LoginPage({}: Props) {
           alt=''/>
       </div>
 
-      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-        <form className='text-white w-full max-w-[300px] rounded-xl mx-auto p-8 px-8 space-y-7'>
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
+      <div className='flex flex-col items-center justify-center px-6 py-8  md:h-screen lg:py-0'>
+        <form className='text-white w-full max-w-[400px] rounded-xl mx-auto p-8 px-8 space-y-8'>
+          <h1 className="flex justify-center items-center text-3xl font-bold leading-tight tracking-tight text-white ">
             Welcome to Calypso
           </h1>
-          <div>
-              <label className="block mb-2 text-sm font-medium text-white ">Your email</label>
-              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@email.com"/>
+
+          <div className='space-y-4'>
+            <div>
+                <label className="block mb-2 text-lg font-medium text-white ">Your email</label>
+                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@email.com"/>
+            </div>
+            <label className="block mb-2 text-lg font-medium text-white">Password</label>
+            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
+            <div className="space-y-4 md:space-y-6">
+              <div className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</div>
+            </div>
           </div>
 
           <div>
-              <label className="block mb-2 text-sm font-medium text-white">Password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
-              <div className="space-y-4 md:space-y-6">
-                <a className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</a>
-              </div>
-          </div>
-
-          <div>
-            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
-            <p className="text-sm font-light text-gray-500">
-                Don't have an account yet? <a className="font-medium text-primary-600 hover:underline ">Sign up</a>
+            <a type="submit" href="/" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-base px-5 py-2.5 text-center">Sign in</a>
+            <p className="flex justify-center items-center text-base space-x-3 font-light text-gray-500">
+              Don't have an account yet? <a href="/register" className="font-medium text-primary-600 hover:underline">Sign up</a>
             </p>
           </div>
       
