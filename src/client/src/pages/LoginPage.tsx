31f17c69518371f982b31e78cf59e18252536f19
your_username <arielleramgoolie27@gmail.com>
2023-06-10 17:18:58 -0400

    Fixed compliation warnings due to login page

--- src/client/src/pages/LoginPage.tsx
+++ src/client/src/pages/LoginPage.tsx
@@ -19,21 +19,21 @@ function LoginPage({}: Props) {
           </h1>
           <div>
               <label className="block mb-2 text-sm font-medium text-white ">Your email</label>
-              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@email.com"/>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@email.com"/>
           </div>
 
           <div>
               <label className="block mb-2 text-sm font-medium text-white">Password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+              <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
               <div className="space-y-4 md:space-y-6">
-                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                <a className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</a>
               </div>
           </div>
 
           <div>
             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
             <p className="text-sm font-light text-gray-500">
-                Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                Don't have an account yet? <a className="font-medium text-primary-600 hover:underline ">Sign up</a>
             </p>
           </div>
       
