341c5b45828a4a132a5573a57e3c5b7edf8045b9
your_username <arielleramgoolie27@gmail.com>
2023-06-10 01:59:33 -0400

    fixed scrolling on login

--- src/client/src/pages/LoginPage.tsx
+++ src/client/src/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 function LoginPage({}: Props) {
   return (
-    <div className='grid bg-darkestGrey text-white h-screen w-full overflow-clip grid-cols-1 sm:grid-cols-2'>
+    <div className='grid bg-darkestGrey text-white h-screen w-full overflow-scroll lg:overflow-clip grid-cols-1 sm:grid-cols-2'>
       <div className='hidden sm:block'>
         <img 
           className='object-cover w-full h-full'
@@ -13,7 +13,7 @@ function LoginPage({}: Props) {
       </div>
 
       <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-        <form className='text-white w-full max-w-[300px] rounded-xl mx-auto p-8 px-8 '>
+        <form className='text-white w-full max-w-[300px] rounded-xl mx-auto p-8 px-8 space-y-7'>
           <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
             Welcome to Calypso
           </h1>
@@ -25,17 +25,18 @@ function LoginPage({}: Props) {
           <div>
               <label className="block mb-2 text-sm font-medium text-white">Password</label>
               <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+              <div className="space-y-4 md:space-y-6">
+                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+              </div>
           </div>
 
-          <div className="space-y-4 md:space-y-6">
-            <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+          <div>
+            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+            <p className="text-sm font-light text-gray-500">
+                Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+            </p>
           </div>
-
-          <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
-          
-          <p className="text-sm font-light text-gray-500">
-              Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-          </p>
+      
 
         </form>
       </div>
