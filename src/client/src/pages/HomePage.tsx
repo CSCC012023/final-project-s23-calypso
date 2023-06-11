246ccc1371785e9e923c49ba6d5154d1bcf72224
William <slicer7667@gmail.com>
2023-06-11 02:17:30 -0400

    Created Header NavBar

--- src/client/src/pages/HomePage.tsx
+++ src/client/src/pages/HomePage.tsx
@@ -1,37 +1,13 @@
 import React from 'react'
+import ExampleNavBar from '../components/common/HeaderNavBar'
 
 type Props = {}
 
 function HomePage({}: Props) {
   return (
-    <header className="bg-blue-500 text-white">
-      <div className="container mx-auto flex items-center justify-between p-4">
-      <h2> Art Marketplace</h2>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <h2> Hello</h2>
-            </li>
-            <li>
-            <h2> Hello</h2>
-            </li>
-            <li>
-            <h2> Hello</h2>
-            </li>
-            <li>
-            <h2> Hello</h2>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-
-
-
-
-
-
-
+    <div className="App">
+      <ExampleNavBar />
+    </div>
   );
 }
 
