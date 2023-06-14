a8a3218e6bc31c5deb9fbe42f87b29b3b26486fb
your_username <arielleramgoolie27@gmail.com>
2023-06-14 18:34:01 -0400

    adjusted text size

--- src/client/src/pages/LoginPage.tsx
+++ src/client/src/pages/LoginPage.tsx
@@ -16,31 +16,30 @@ function LoginPage() {
         </div>
 
         <div className='flex flex-col items-center justify-center px-6 py-8  md:h-screen lg:py-0'>
-          <img className="h-20 flex justify-center items-center" src={logo.img} />
-          <form className='text-white w-full max-w-[400px] rounded-xl mx-auto p-8 px-8 space-y-8'>
-            <h1 className="flex justify-center items-center text-3xl font-bold leading-tight tracking-tight text-white ">
-              Welcome to Calypso
-            </h1>
-
+          <img className="h-20" src={logo.img} />
+          <form className='text-white w-full max-w-[500px] rounded-xl mx-auto p-8 px-8 space-y-5'>
             <div className='space-y-4'>
               <div>
-                  <label className="block mb-2 text-xl font-medium text-white ">Email</label>
-                  <input type="email" name="email" id="email" className="bg-black border-black text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@email.com"/>
+                <label className="block mb-2 text-2xl font-medium text-white ">Email</label>
+                <input type="email" name="email" id="email" className="bg-black border-black text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full h-[3rem] p-2.5 " placeholder="name@email.com"/>
               </div>
-              <label className="block mb-2 text-xl font-medium text-white">Password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••" className="bg-black border border-black text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
-              <div className="space-y-4 md:space-y-6">
-                <div className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</div>
+              <div>
+                <label className="block mb-2 text-2xl font-medium text-white">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-black border border-black text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full h-[3rem] p-2.5"/>
+              </div>
+              
+              <div className="space-y-3 md:space-y-6">
+                <div className="text-medium font-medium text-primary-600 hover:underline">Forgot password?</div>
               </div>
             </div>
 
-            <div className='space-y-0 space-x-1'>
-              <a type="submit" href="/" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-base px-5 py-2.5 text-center">Sign in</a>
-              <div className="flex justify-center items-center text-base space-x-3 font-light text-gray-500">
-                <p >
+            <div>
+              <a type="submit" href="/" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center">Sign in</a>
+              <div className="flex justify-center items-center space-x-3 font-light text-gray-500">
+                <p className='text-base'>
                   Don't have an account yet? 
                 </p>
-                <a href="/register" className="font-medium text-primary-600 hover:underline">Sign up</a>
+                <a href="/register" className="font-medium text-primary-600 text-base hover:underline">Sign up</a>
               </div>
             </div>
 
