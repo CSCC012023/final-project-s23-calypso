2bc0d93642095ba7a3c9b87fbd9e1df3e8948ee8
William <73299741+williamc99@users.noreply.github.com>
2023-06-14 17:23:05 -0400

    Update HomePage.tsx

--- src/client/src/pages/HomePage.tsx
+++ src/client/src/pages/HomePage.tsx
@@ -104,12 +104,11 @@ function HomePage() {
         <div className="relative max-w-3xl mx-auto py-32 px-6 flex flex-col items-center text-center sm:py-64 lg:px-0">
           <h1 className="text-4xl font-extrabold tracking-tight text-white lg:text-6xl">New arrivals are here</h1>
           <p className="mt-4 text-xl text-white">
-            The new arrivals have, well, newly arrived. Check out the latest options from our summer small-batch release
-            while they're still in stock.
+            New music, new artworks, new artist, new everything.  Check out the new arrivals on Calypso now!
           </p>
           <a
             href="new"
-            className="mt-8 inline-block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100"
+            className="mt-8 inline-block bg-white border border-transparent rounded-md py-3 px-8 text-lg font-medium text-gray-900 hover:bg-gray-200"
           >
             Shop New Arrivals
           </a>
