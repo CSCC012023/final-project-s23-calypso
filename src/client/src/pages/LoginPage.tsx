2ce368a1bc92bd49dd86b464c728a81477d8b208
your_username <arielleramgoolie27@gmail.com>
2023-06-14 19:56:21 -0400

    Updated register page and small edit on login

--- src/client/src/pages/LoginPage.tsx
+++ src/client/src/pages/LoginPage.tsx
@@ -35,7 +35,7 @@ function LoginPage() {
 
             <div>
               <a type="submit" href="/" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center">Sign in</a>
-              <div className="flex justify-center items-center space-x-3 font-light text-gray-500">
+              <div className="flex justify-center items-center space-x-1 font-light text-gray-500">
                 <p className='text-base'>
                   Don't have an account yet? 
                 </p>
