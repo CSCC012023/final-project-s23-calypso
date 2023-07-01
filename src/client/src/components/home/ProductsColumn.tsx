734ef267c37edb1971ddfab8cfd7d81006d5b484
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-07-01 03:21:26 -0400

    Make significant changes to product page

---
+++ src/client/src/components/home/ProductsColumn.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import SmallProductCard from "./SmallProductCard";
+import LargeProductCard from "./LargeProductCard";
+
+
+interface Props {
+    categoryTitle: string,
+    productsList: any,
+}
+
+export default function ProductsRow( {productsList, categoryTitle}: Props ) {
+    return (
+        <div className="">
+            <div className="py-48 lg:max-w-7xl sm:py-8 lg:py-24 lg:px-8">
+            <div className="px-4 items-center justify-center mx-40 sm:px-6 lg:px-0">
+                <h2 id="trending-heading" className="text-center text-6xl font-extrabold tracking-tight text-gray-100">
+                    {categoryTitle}
+                </h2>
+            </div>
+
+            <div className="mt-8 flex justify-center">
+                <div className="w-full max-w-screen-lg">
+                {productsList.map((product: any) => (
+                    <div key={product.id} className="my-4">
+                        <LargeProductCard productProp={product} />
+                        <div className="flex justify-center mt-4 mb-24">
+                            <button className="bg-gray-600 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mr-2">
+                                Add to cart
+                            </button>
+                            <button className="bg-gray-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded ml-2">
+                                Place a bid
+                            </button>
+                        </div>
+                    </div>
+                ))}
+                </div>
+            </div>
+            </div>
+        </div>
+
+    );
+};
--- src/client/src/pages/ProductPage.tsx
+++ src/client/src/pages/ProductPage.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
 import ExampleNavBar from '../components/common/HeaderNavBar';
-
+import previewArt from '../assets/previewArt.jpg'
+import samplePanda from '../assets/panda.png'
+import sampleProductImage from '../assets/sampleProductImage.png'
+import sampleProductImage2 from '../assets/sampleProductImage2.jpg'
+import sampleLargeProductImage from '../assets/sampleLargeProductImage.jpg'
+import sampleLargeProductImage2 from '../assets/sampleLargeProductImage2.jpg'
+import sampleProfilePicture1 from '../assets/sampleProfilePicture1.png'
+import ProductsColumn from '../components/home/ProductsColumn';
 type Props = {};
+
+const artworks = [
+  {
+    id: 1,
+    name: 'Lost Girl',
+    artist: 'Jennie Li',
+    style: 'Oil on canvas',
+    price: '$500',
+    href: 'product',
+    imageSrc: sampleProductImage2,
+    imageAlt: 'LOST GIRL - JENNIE LI',
+  },
+  {
+    id: 2,
+    name: 'Dystopian Future',
+    artist: 'Markus Lawerence',
+    style: 'Digital',
+    price: '$3000',
+    href: '#',
+    imageSrc: sampleLargeProductImage,
+    imageAlt: 'DYSTOPIAN FUTURE - MARKUS LAWERENCE',
+  },
+  {
+    id: 3,
+    name: 'Fox-Masked Boy',
+    artist: 'Natalie Hall',
+    style: 'Watercolor on paper',
+    price: '$50',
+    href: '#',
+    imageSrc: sampleProfilePicture1,
+    imageAlt: 'FOX MASKED BOY - NATALIE HALL',
+  },
+  {
+    id: 4,
+    name: 'Panda',
+    artist: 'Panda Man',
+    style: 'Sculpture',
+    price: '$900',
+    href: '#',
+    imageSrc: samplePanda,
+    imageAlt: 'PANDA - PANDA MAN',
+  },
+]
 
 function ProductPage({}: Props) {
   const previewArtPanel = {
@@ -10,9 +60,12 @@ function ProductPage({}: Props) {
   };
 
   return (
-    <div className="flex flex-col bg-darkestGrey h-screen w-screen">
+    <div className="flex flex-col bg-darkestGrey min-h-screen">
       <ExampleNavBar />
-      <div className="flex flex-col items-center">
+      <div className="flex justify-center items-center">
+        <ProductsColumn productsList={artworks} categoryTitle="Available Products" />
+      </div>
+      {/* <div className="flex flex-col items-center">
         <div className = "justify-center"
           style={{
             width: '900px',
@@ -41,15 +94,8 @@ function ProductPage({}: Props) {
             <p className="text-black">A Maaneth De Silva Original piece !</p>
           </div>
         </div>
-        <div className="flex justify-center mt-4">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
-            Add to cart
-          </button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2">
-            Place a bid
-          </button>
-        </div>
-      </div>
+
+      </div> */}
     </div>
   );
 }
