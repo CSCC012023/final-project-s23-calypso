4a8a388d0ea8601eba41d67b6348674378df7afd
William <slicer7667@gmail.com>
2023-07-13 01:42:55 -0400

    CAL55 Add Sort Method/Queries to Backend

--- src/server/models/neo4j/Artwork.js
+++ src/server/models/neo4j/Artwork.js
@@ -1,11 +1,22 @@
 //Model file for Artwork related queries
 
-const findAll = async (session) => {
-    const result = await session.run('MATCH (n: Artwork) RETURN n');
-    return result.records.map(i => i.get('n').properties);
+const getArtworks = async (session, sortParam) => {
+    let query = '';
+
+    if (sortParam === '') {
+        query = `MATCH (n: Artwork) RETURN n`;
+    }
+    else{
+        query = `MATCH (n: Artwork) RETURN n ORDER BY n.${sortParam}`;
+    }
+    
+
+    const result = await session.run(query);
+    artworks = result.records.map(i => i.get('n').properties);
+    return artworks;
 }
 
 
 module.exports = {
-    findAll: findAll,
+    getArtworks: getArtworks,
 }
