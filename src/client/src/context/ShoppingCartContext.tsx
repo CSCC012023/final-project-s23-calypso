b637c253c629e7f383649caeeca5de6732278fd5
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-07-18 17:55:54 -0400

    cal-125 Add context for shopping cart

---
+++ src/client/src/context/ShoppingCartContext.tsx
@@ -0,0 +1,20 @@
+import {useContext, createContext, ReactNode} from 'react';
+import React from 'react';
+
+type ShoppingCartProviderProps = {
+    children: ReactNode;
+}
+
+const ShoppingCartContext = createContext({}) 
+
+export function useShoppingCart() {
+    return useContext(ShoppingCartContext)
+}
+
+export function ShoppingCartProvider( {children}: ShoppingCartProviderProps) {
+    return (
+        <ShoppingCartContext.Provider value={{}}>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
+}
