12107ac36be67457c59add155dff39e289602321
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-07-18 17:57:15 -0400

    cal-125 Add comments to shopping cart context

--- src/client/src/context/ShoppingCartContext.tsx
+++ src/client/src/context/ShoppingCartContext.tsx
@@ -1,18 +1,23 @@
-import {useContext, createContext, ReactNode} from 'react';
+import { useContext, createContext, ReactNode } from 'react';
 import React from 'react';
 
+// Define the type for the props of the ShoppingCartProvider component
 type ShoppingCartProviderProps = {
     children: ReactNode;
 }
 
-const ShoppingCartContext = createContext({}) 
+// Create a new context for the shopping cart data
+const ShoppingCartContext = createContext({})
 
+// Custom hook to access the shopping cart data from the context
 export function useShoppingCart() {
     return useContext(ShoppingCartContext)
 }
 
-export function ShoppingCartProvider( {children}: ShoppingCartProviderProps) {
+// ShoppingCartProvider component responsible for providing the shopping cart data to its children
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
     return (
+        // Provide the shopping cart data through the context
         <ShoppingCartContext.Provider value={{}}>
             {children}
         </ShoppingCartContext.Provider>
