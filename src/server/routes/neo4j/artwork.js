b4ee5a7948d34ceffb2fc150eef2e2441647aabe
maanethdesilva <maaneth.2002@gmail.com>
2023-07-21 23:34:51 +0530

    Added api function to recommend new art

--- src/server/routes/neo4j/artwork.js
+++ src/server/routes/neo4j/artwork.js
@@ -7,5 +7,8 @@ const artworkController = require('../../controllers/neo4j/artworkController');
 
 artwork.get('/all', artworkController.getArtworks);
 
+artwork.get('/recommended/:id', artworkController.getRecommendedArtworks);
+
+
 
 module.exports = artwork;
