d934f11761d57f1cb06d433e0ed5a504b7d326c5
your_username <arielleramgoolie27@gmail.com>
2023-07-26 21:24:33 -0400

    Updated small login page bug

--- src/client/src/pages/LoginPage2.tsx
+++ src/client/src/pages/LoginPage2.tsx
@@ -36,7 +36,7 @@ function LoginPage2() {
         console.log('Response worked!');
         const data = await response.json();
         setCookie('token', data.token , { path: '/' })
-        navigate("/")
+        navigate("/home")
       }
       else if (response.status == 403){
         console.log('Response failed!');
