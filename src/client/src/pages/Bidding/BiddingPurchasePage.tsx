e7006271ad2284dc06c2416b226e8d6888ac1bec
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-07-26 20:21:46 -0400

    CAL-54 Make ui adjustments

--- src/client/src/pages/Bidding/BiddingPurchasePage.tsx
+++ src/client/src/pages/Bidding/BiddingPurchasePage.tsx
@@ -3,7 +3,7 @@ import Menu from '../../components/Menu'
 
 type Props = {}
 
-function BiddingPage({}: Props) {
+function BiddingPurchasePage({}: Props) {
 
   const previewArtPanel = {
     img: require("../../assets/previewArt.jpg"),
@@ -24,30 +24,30 @@ function BiddingPage({}: Props) {
     <div className="flex bg-darkestGrey text-white h-screen overflow-clip">
       <Menu />
       <div className="bg-darkestGrey text-white h-screen w-full px-10 mx-auto py-10 space-y-5 overflow-scroll">
-      <div>
-        <a href="/bidding" className="bg-darkGrey text-white text-center font rounded-lg text-2xl px-5 py-3 space-y-5">{"<"}</a>
-      </div>
+        <div>
+          <a href="/bidding" className="bg-darkGrey text-white text-center font rounded-lg text-2xl px-2 py-2 space-y-5">{"<"}</a>
+        </div>
         <h1 className="text-4xl font-semibold">Art Name</h1>
         <div className="flex items-center">
-          <img className="h-1/12 w-1/12 rounded-full" src={creatorPanel.img}/>
+          <img className="h-1/12 w-1/12 rounded-full" src={creatorPanel.img} alt="Creator" />
           <p className="text-2xl font-semibold mx-8">Creator</p>
         </div>
-        
-        <img className="h-1/3 w-full object-cover" src={previewArtPanel.img} />
+
+        <img className="h-1/3 w-full object-cover" src={previewArtPanel.img} alt="Preview Art" />
         <div className="bg-darkGrey rounded-lg p-5 space-y-5">
           <p className="text-xl font-semibold mb-40">{descriptionPanel.description}</p>
           <p className="text-xl font-semibold">{descriptionPanel.price}</p>
         </div>
-        <div className="rounded-lg p-5 space-y-5">
-          <p className="text-xl font-semibold"> Bid Amount</p>
-          <div className="flex justify-between">
-            <input className="text-black rounded-lg p-5 space-y-5" type="text" placeholder="Enter Bid Amount" pattern="[0-9]+"/>
-            <button className="bg-white text-black text-center rounded-sm p-5 space-y-5">Place Bid</button>
+        <div className="rounded-lg p-5 space-y-5 text-xl">
+          <p className="text-xl font-semibold">Bid Amount</p>
+          <div className="flex flex-col md:flex-row justify-between space-y-4 md:space-y-0 md:space-x-4">
+            <input className="text-black rounded-xl p-4" type="text" placeholder="Enter Bid Amount" pattern="[0-9]+" />
+            <button className="bg-white text-black text-center rounded-xl p-4">Place Bid</button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default BiddingPage
+export default BiddingPurchasePage
